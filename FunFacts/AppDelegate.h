6777b50efa6dca9ea42905eed70a4db12940435f
Alp Eren Can <alperenc@gmail.com>
2015-06-16 00:14:57 +0300

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Alp Eren Can on 16/06/15.
+//  Copyright (c) 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Alp Eren Can on 16/06/15.
+//  Copyright (c) 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
